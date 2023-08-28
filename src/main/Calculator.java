3cd349c059396d88d942f8f57889d2f764beb635
Gabriel Vitor <gabrielvitor0309@gmail.com>
2023-08-28 18:42:25 -0300

    Colocando o código da classe Calculator

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Calculator {
+	private double resultado;
 
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
 }
