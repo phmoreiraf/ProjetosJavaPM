a0e266ce64af32a54e58030d3b70df0f4d299148
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-30 21:37:05 -0300

    Update Calculator.java

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -11,15 +11,15 @@ public class Calculator {
 		return a - b;
 	}
 
-	    public double raiz(double num1) {
+ public double raiz(double num1) {
 
-        return 0;
+        return Math.sqrt(num1);
 
     }
 
     public double potenciar(double num1, double num2) {
 
-        return 0;
+        return Math.pow(num1, num2);
 
     }
 }
