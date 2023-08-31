091403c1389325eabe8ab222fbfaa2ec8252fd23
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-31 09:21:19 -0300

    Update Operacoes.java

--- TesteUnitario/testeunitario/src/main/java/tdd2/Operacoes.java
+++ TesteUnitario/testeunitario/src/main/java/tdd2/Operacoes.java
@@ -1,1 +1,18 @@
 
+package tdd2;
+
+public class Operacoes {
+
+    public double raiz(double num1) {
+
+        return 0;
+
+    }
+
+    public double potenciar(double num1, double num2) {
+
+        return 0;
+
+    }
+
+}
