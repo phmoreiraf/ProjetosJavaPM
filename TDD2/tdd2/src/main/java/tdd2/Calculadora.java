cfdbb3754114848982b7a1ffdc82c6682223582d
Arthur Bretas <arthurcbretas@gmail.com>
2023-08-31 15:55:26 -0300

    Implementação no main

--- TDD2/tdd2/src/main/java/tdd2/Calculadora.java
+++ TDD2/tdd2/src/main/java/tdd2/Calculadora.java
@@ -1,73 +1,81 @@
 
 package tdd2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Calculadora {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         Operacoes realizar = new Operacoes();
 
-        double num1, num2, angulo;
+        int escolha;
         double resultado;
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Calculadora");
-            System.out.println("Escolha a operação:");
-            System.out.println("7 - Tangente");
-            System.out.println("8 - Raiz Quadrada");
-            System.out.println("9 - Potenciação");
-            System.out.println("0 - Sair");
+            do {
+                System.out.println("Calculadora");
+                System.out.println("1 - Soma");
+                System.out.println("2 - Subtração");
+                System.out.println("3 - Multiplicação");
+                System.out.println("4 - Divisão");
+                System.out.println("5 - Seno");
+                System.out.println("6 - Cosseno");
+                System.out.println("7 - Tangente");
+                System.out.println("8 - Raiz Quadrada");
+                System.out.println("9 - Potenciação");
+                System.out.println("0 - Sair");
+                System.out.println("Escolha uma operação:");
+                escolha = scanner.nextInt();
+                scanner.nextLine();
+                switch (escolha) {
+                    case 1:
 
-            int escolha = scanner.nextInt();
+                        break;
+                    case 2:
 
-            if (escolha == 0) {
-                System.out.println("Encerrando a calculadora...");
-                System.exit(escolha);
-                break;
-            }
+                        break;
+                    case 3:
 
-            switch (escolha) {
-                case 1:
+                        break;
+                    case 4:
 
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        System.out.print("Digite um valor para calcular sua tangente: ");
+                        double tan = scanner.nextDouble();
+                        resultado = realizar.tangente(tan);
+                        System.out.printf("A tangente de %.2f é, aproximadamente, %.2f.\n", tan, resultado);
+                        break;
+                    case 8:
+                        System.out.print("Digite o número: ");
+                        double numero = scanner.nextDouble();
+                        resultado = realizar.raiz(numero);
+                        System.out.printf("Raiz Quadrada de %.2f é, aproximadamente, %.2f.\n", numero, resultado);
+                        break;
+                    case 9:
+                        System.out.print("Digite a base: ");
+                        double base = scanner.nextDouble();
+                        System.out.print("Digite o expoente: ");
+                        double expoente = scanner.nextDouble();
+                        resultado = realizar.potenciar(base, expoente);
+                        System.out.println("Potenciação: " + resultado);
+                        break;
+                    default:
+                        if(escolha == 0){
+                            break;
+                        }
+                        System.out.println("Opção inválida!\nPor favor, escolha uma opção válida.");
 
-                case 2:
-
-
-                case 3:
-
-
-                case 4:
-
-
-                case 5:
-
-                case 6:
-
-                case 7:
-
-
-               case 8:
-                    System.out.print("Digite o número: ");
-                    double numero = scanner.nextDouble();
-                    resultado = realizar.raiz(numero);
-                    System.out.println("Raiz Quadrada de : " + numero + "sera: " + resultado);
-                    break;
-                case 9:
-                    System.out.print("Digite a base: ");
-                    double base = scanner.nextDouble();
-                    System.out.print("Digite o expoente: ");
-                    double expoente = scanner.nextDouble();
-                    resultado = realizar.potenciar(base, expoente);
-                    System.out.println("Potenciação: " + resultado);
-                    break;
-                default:
-                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
-            }
-        }
-
+                        break;
+                }
+            }while(escolha != 0);
+        System.out.println("Encerrando a calculadora...");
+        System.exit(escolha);
         scanner.close();
     }
 }
