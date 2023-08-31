1bc174a77a54170dd481e3f33d047cefeac6b77b
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-31 09:21:58 -0300

    Update CalculadoraTest.java

--- TesteUnitario/testeunitario/src/test/java/tdd2/CalculadoraTest.java
+++ TesteUnitario/testeunitario/src/test/java/tdd2/CalculadoraTest.java
@@ -1,1 +1,24 @@
+package tdd2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculadoraTest {
+
+   private Operacoes calcular = new Operacoes();
+  
+    @Test
+    public void testRaiz() {
+        double raiz = calcular.raiz(81);
+        assertEquals(9, raiz, 0.01);
+    }
+
+    @Test
+    public void testPotencia() {
+        double potencia = calcular.potenciar(10,2);
+        assertEquals(100, potencia, 0.01);
+    }
+
+}
 
