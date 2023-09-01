72d078a4bf0ae7ef28a98fe89ef5c1ae0fac2172
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-31 21:29:51 -0300

    Update Calculadora.java

--- TDD2/tdd2/src/main/java/tdd2/Calculadora.java
+++ TDD2/tdd2/src/main/java/tdd2/Calculadora.java
@@ -47,26 +47,11 @@ public class Calculadora {
                     System.out.println("Resultado: " + resultSub);
 
                         break;
-              case 3:
-                    System.out.print("");
-                    System.out.print("Digite o primeiro número: ");
-                    num1 = scanner.nextDouble();
-                    System.out.print("Digite o segundo número: ");
-                    num2 = scanner.nextDouble();
+                    case 3:
 
-                    resultado = realizar.multiplicar(num1, num2);
-                    System.out.println("Resultado: " + resultado);
                     break;
-                case 4:
+                    case 4:
 
-                    System.out.print("");
-                    System.out.print("Digite o primeiro número: ");
-                    num1 = scanner.nextDouble();
-                    System.out.print("Digite o segundo número: ");
-                    num2 = scanner.nextDouble();
-
-                    resultado = realizar.dividir(num1, num2);
-                    System.out.println("Resultado: " + resultado);
                     break;
                     case 5:
                         System.out.println("Digite um valor para calcular seu seno: ");
