fcfe7e6dd95ed0a4e7650cb5dd44c6a4c1630ac7
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-31 21:22:36 -0300

    Update Operacoes.java

--- TDD2/tdd2/src/main/java/tdd2/Operacoes.java
+++ TDD2/tdd2/src/main/java/tdd2/Operacoes.java
@@ -11,15 +11,15 @@ public class Operacoes {
         return a - b ;
      }
 
-        public double dividir(double num1, double num2) {
+       public double dividir(double num1, double num2) {
 
-        return 0;
+        return num1 / num2;
 
     }
 
     public double multiplicar(double num1, double num2) {
 
-        return 0;
+        return num1 * num2;
 
     }
 
